46063d2cb127ab5b4df4a4431082d890a9163787
douglas@budadigital.com.br <douglas@budadigital.com.br>
2020-03-30 13:43:35 -0300

    Deploy
    up

---
+++ build/precache-manifest.84942a5f71b34b9a412c44d3030785ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aafa2a79c3e859edbc53a960311f2ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf926212ef65a99bd717",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0c7209b308bd3d0970f0",
+    "url": "/static/js/2.b30c45fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b30c45fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf926212ef65a99bd717",
+    "url": "/static/js/main.071632f8.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
