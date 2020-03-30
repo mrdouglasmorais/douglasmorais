a4e8727af8c1f46ccef60e90e1245e9028cf819a
douglas@budadigital.com.br <douglas@budadigital.com.br>
2020-03-30 19:06:12 -0300

    UpLoad
    Up

---
+++ src/components/AsideLeft/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AsideLeft() {
+
+  const name = `< Douglas Morais />`
+  return (
+    <aside className="asideass">
+      <h2>{name}</h2>
+    </aside>
+  )
+}
